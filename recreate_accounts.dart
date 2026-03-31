1d456e5b8eee5c58f497f1578a3f0f84411da755
Naresh Kumar Vaid <148624421+nareshved@users.noreply.github.com>
2026-03-31 19:23:27 +0530

    remove urls my admin

--- recreate_accounts.dart
+++ recreate_accounts.dart
@@ -2,8 +2,8 @@ import 'dart:convert';
 import 'dart:io';
 
 void main() async {
-  final url = Uri.parse('https://ygtxsykpihqbzzgbqyew.supabase.co/auth/v1/signup');
-  final anonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlndHhzeWtwaWhxYnp6Z2JxeWV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQxMzg2OTksImV4cCI6MjA4OTcxNDY5OX0.htrQPKADyuUE5azY-WZkY7Aii2NBdsBs_5_jakHOMzI';
+  final url = Uri.parse('https://ygtxsy');
+  final anonKey = 'eyJhbGciOiJIUHOMzI';
   
   final client = HttpClient();
   
